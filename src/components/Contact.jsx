d5d417d02aee454ceb63c4b35421adba5df6f656
Saphal Sherchan <saphal@outlook.sg>
2022-09-18 18:37:16 +0545

    scrollbutton

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { BsArrowDownShort } from "react-icons/bs";
+import { Link } from "react-scroll";
 
 const Contact = () => {
+  const [arrow,setArrow] = useState(false);
+  const arrowClick = () => {
+    setArrow(!arrow);
+  }
   return (
     <div name="contact" className="w-full h-screen bg-[#0c1c0e] w-full h-screen flex justify-center items-center p-4 ">
         <form method='POST' action='https://getform.io/f/50f92bd4-2033-448f-8534-c1e833bc163e' className='flex flex-col max-w-[600px] w-full'>
@@ -13,6 +19,13 @@ const Contact = () => {
             <textarea className='bg-[#d0f9d5] p-2' rows='10' name='message' placeholder='Message'/>
             <button className='text-white border-2 my-8 mx-auto py-3 px-4 hover:bg-yellow-500 hover:border-yellow-500 flex items-center'>Let's Collaborate!</button>
         </form>
+        <span className=" duration-300 ">
+        <Link onClick={arrowClick} to={`${arrow ? "home" : "contact" }`} smooth={true} duration={900}>
+              <BsArrowDownShort className={`bottom-10 right-10 fixed  rounded-full w-10 h-10 bg-green-500 border-none
+                ${arrow ? 'rotate-180' : ''}
+              `} />
+              </Link>
+            </span>
     </div>
   )
 }
